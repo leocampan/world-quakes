aa4bf766e450438d7634cb854993a144477ff0c9
leocampan <leonardo.campan@icloud.com>
2021-11-04 12:55:07 +0100

    04.11.2021 (1)

--- js/script.js
+++ js/script.js
@@ -8,17 +8,19 @@ $(document).ready(function() {
     }).then(function(data) {
        //console.log(data.features.length);
        var nDati;
-       var s="<table border='1'>";
-       s+='<tr><td>luogo</td><td>latitudine</td><td>longitudine</td></tr>';
-       nDati=data.features.length;
-	   for (i=0; i < data.features.length;i++)
-	   {
-		  // console.log(data.features[i].properties.place);
-		   s+='<tr><td>'+data.features[i].properties.place+'</td><td>'+data.features[i].geometry.coordinates[0]+'</td><td>'+data.features[i].geometry.coordinates[1]+'</td></tr>';
-           }
-           s+='</table>';
-	   //console.log(data);
-	   $('#totale').append(nDati);
-       $('#contenuto').append(s);
+       var s = "<table border= collapse>";
+       s += '<tr><td>Place</td><td>Latitude</td><td>Longitude</td><td>Intencity</td></tr>';
+       nDati = data.features.length;
+
+	   for (var i = 0; i < data.features.length; i++) {
+		    console.log(data.features[i].properties.place);
+		    s += '<tr><td>'+data.features[i].properties.place+'</td><td>'+data.features[i].geometry.coordinates[0]+'</td><td>'+data.features[i].geometry.coordinates[1]+'</td><td>' + 
+            data.features.properties.title +'</td></tr>';
+        }
+        
+        s+='</table>';
+	    //console.log(data);
+	    $('#totale').append(nDati);
+        $('#contenuto').append(s);
     });
 });
