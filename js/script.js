2cd9dc2cf8c7bf3da2e51ef7b5024216ea6df5e8
leocampan <leonardo.campan@icloud.com>
2021-12-06 22:40:36 +0100

    06.12.2021 (3)

--- js/script.js
+++ js/script.js
@@ -1,29 +1,24 @@
-window.onload = function(){createTable()};
+window.onload = function () { createTable() };
 
 function createTable() {
-    window.open("http://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=2014-01-01&endtime=2014-01-02").close();
-    $(document).ready(function() {
-    $.ajax({
-        //url: "http://rest-service.guides.spring.io/greeting"
-        url: "http://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=2014-01-01&endtime=2014-01-02"
-            //url: "http://earthquake.usgs.gov/fdsnws/event/1/version"
-            //url: "http://earthquake.usgs.gov/fdsnws/event/1/count?starttime=2014-01-01&endtime=2014-01-02"
-            //url: "http://earthquake.usgs.gov/fdsnws/event/1/count?format=geojson"
-    }).then(function(data) {
-        //console.log(data.features.length);
-        var nDati;
-        var s = "<table border='1'>";
-        s += '<tr>  <td>Place </td> <td>Laitude</td> <td>Longiude</td> </tr>';
-        nDati = data.features.length;
+    $(document).ready(function () {
+        $.ajax({
+            url: "http://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=2014-01-01&endtime=2014-01-02"
+        }).then(function (data) {
+            //console.log(data.features.length);
+            var nDati;
+            var s = "<table border='1'>";
+            s += '<tr>  <td>Place </td> <td>Laitude</td> <td>Longiude</td> </tr>';
+            nDati = data.features.length;
 
-        for (i = 0; i < data.features.length; i++) {
-            // console.log(data.features[i].properties.place);
-            s += '<tr><td>' + data.features[i].properties.place + '</td><td>' + data.features[i].geometry.coordinates[0] + '</td><td>' + data.features[i].geometry.coordinates[1] + '</td></tr>';
-        }
-        s += '</table>';
-        //console.log(data);
-        $('#totale').append(nDati);
-        $('#contenuto').append(s);
+            for (i = 0; i < data.features.length; i++) {
+                // console.log(data.features[i].properties.place);
+                s += '<tr><td>' + data.features[i].properties.place + '</td><td>' + data.features[i].geometry.coordinates[0] + '</td><td>' + data.features[i].geometry.coordinates[1] + '</td></tr>';
+            }
+            s += '</table>';
+            //console.log(data);
+            $('#totale').append(nDati);
+            $('#contenuto').append(s);
+        });
     });
-});
 }
