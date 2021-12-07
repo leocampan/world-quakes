1c2b1295f642727e81098774010f57cfc9ad0618
leocampan <leonardo.campan@icloud.com>
2021-12-07 18:56:43 +0100

    07.12.2021 (2)

--- js/script.js
+++ js/script.js
@@ -1,24 +1,31 @@
-window.onload = function () { createTable() };
+window.onload = function () {
+  createTable();
+};
 
 function createTable() {
-    $(document).ready(function () {
-        $.ajax({
-            url: "http://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=2014-01-01&endtime=2014-01-02"
-        }).then(function (data) {
-            //console.log(data.features.length);
-            var nDati;
-            var s = "<table border='1'>";
-            s += '<tr>  <td>Place </td> <td>Laitude</td> <td>Longiude</td> </tr>';
-            nDati = data.features.length;
+  $(document).ready(function () {
+    $.ajax({
+      url: "http://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=2014-01-01&endtime=2014-01-02",
+    }).then(function (data) {
+      var nDati;
+      var s = "<table border='1'>";
+      s += "<tr>  <td>Place </td> <td>Laitude</td> <td>Longiude</td> </tr>";
+      nDati = data.features.length;
 
-            for (i = 0; i < data.features.length; i++) {
-                // console.log(data.features[i].properties.place);
-                s += '<tr><td>' + data.features[i].properties.place + '</td><td>' + data.features[i].geometry.coordinates[0] + '</td><td>' + data.features[i].geometry.coordinates[1] + '</td></tr>';
-            }
-            s += '</table>';
-            //console.log(data);
-            $('#total').append(nDati);
-            $('#content').append(s);
-        });
+      for (i = 0; i < data.features.length; i++) {
+        s +=
+          "<tr><td>" +
+          data.features[i].properties.place +
+          "</td><td>" +
+          data.features[i].geometry.coordinates[0] +
+          "</td><td>" +
+          data.features[i].geometry.coordinates[1] +
+          "</td></tr>";
+      }
+      
+      s += "</table>";
+      $("#totale").append(nDati);
+      $("#contenuto").append(s);
     });
+  });
 }
