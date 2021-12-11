f3af47cb385ca7c236a73b17f6d140e398b25c67
leocampan <leonardo.campan@icloud.com>
2021-12-11 17:51:46 +0100

    11.12.2021 (1)

--- js/script.js
+++ js/script.js
@@ -5,11 +5,11 @@ window.onload = function () {
 function createTable() {
   $(document).ready(function () {
     $.ajax({
-      url: "http://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=2014-01-01&endtime=2014-01-02",
+      url: "http://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=2021-01-01&endtime=2022-01-02",
     }).then(function (data) {
       var nDati;
       var s = "<table border='1'>";
-      s += "<tr>  <td>Place </td> <td>Laitude</td> <td>Longiude</td> </tr>";
+      s += "<tr> <td>Place </td> <td>Laitude</td> <td>Longiude</td> </tr>";
       nDati = data.features.length;
 
       for (i = 0; i < data.features.length; i++) {
@@ -24,8 +24,15 @@ function createTable() {
       }
       
       s += "</table>";
-      $("#totale").append(nDati);
-      $("#contenuto").append(s);
+      $("#results").append(nDati);
+      $("#content").append(s);
     });
   });
+}
+
+let btn = document.querySelector("button");
+let sidebar = document.querySelector(".sidebar");
+
+btn.onclick = function() {
+  btn.classList.toggle("active");
 }
