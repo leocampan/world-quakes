015055e99dc64f6b22d9a74cf66a6c4602fefe76
leocampan <leonardo.campan@icloud.com>
2022-01-12 23:04:46 +0100

    finalcimmit

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+window.onload = function(){tabellaoggi()};
+
 function days() {
   var yearMonth = document.getElementById("month").value; // yyyy-mm
   var month = yearMonth.slice(5, 7); // mm
@@ -45,49 +47,163 @@ function days() {
   return days;
 }
 
-function createTable() {
+function tabellaoggi() {
   $(document).ready(function () {
-    var yearMonth = document.getElementById("month").value; // yyyy-mm
+    var today = new Date();
+    var todaydate = today.getFullYear() + '-' + (today.getMonth() + 1)+'-'+today.getDate(); // yyyy-mm-dd
+
+    var todayyearmonth = todaydate.slice(0, 6); // yyyy-mm
+    var todayday = todaydate.slice(7, 10); // dd
 
     var url =
-      "http://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=" + yearMonth + "-01&endtime=" + yearMonth + "-" + days();
-      // "http://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=2021-01-01&endtime=2021-01-02"
-
+      "http://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=" + todayyearmonth + "-01&endtime=" + todayyearmonth + "-" + todayday;
+    // "http://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=2021-01-01&endtime=2021-01-02"
     $.ajax({
       url,
     }).then(function (data) {
+      puntitrovati=new Array();
       var nDati;
-      var s = "<table border='1'>";
-      s +=
-        "<tr> <th>Place </th> <th>Laitude</th> <th>Longiude</th> <th>Magnitude</th></tr>";
+      var s = "<table border= 1px>";
+      s += "<tr> <th>Date</th> <th>Place</th> <th>Magnitude</th></tr>";
       nDati = data.features.length;
+      var nrighe = 0;
 
-      var magnitude = document.getElementById("magnitude").value; 
+      for (i = 0; i < nDati; i++) {
+        var sec = data.features[i].properties.time;
+        var dateComplete = new Date(sec);
+        var date = dateComplete.toString().slice(3, 24);
+        var mag = document.getElementById("magnitude").value;
+        var valMag = parseFloat(mag);
+        var range = document.getElementById("range").value;
+        var valRange =  parseFloat(range);
+        var stato = document.getElementById("state").toUpperCase;
 
-      while (i = 0 && i < data.features.length) {
-        if (data.features[i].properties.mag < (data.features[i].properties.mag + magnitude) && data.features[i].properties.mag > (data.features[i].properties.mag - magnitude)) {
-          s +=
-          "<tr><td>" +
-          data.features[i].properties.place +
-          "</td><td>" +
-          data.features[i].geometry.coordinates[0] +
-          "</td><td>" +
-          data.features[i].geometry.coordinates[1] +
-          "</td><td>" +
-          data.features[i].properties.mag +
-          "</td></tr>";
-        }
-        else {
-          i++;
-        }   
+        s += "<tr><td>" + 
+        date +"</td><td>" + 
+        data.features[i].properties.place + "</td><td>" +
+        data.features[i].properties.mag + "</td></tr>";
+        nrighe ++;
+        puntitrovati.push(data.features[i].geometry.coordinates[0])
+        puntitrovati.push(data.features[i].geometry.coordinates[1]) 
       }
-
-      s += "</table>";
-
-      document.getElementById("results").innerHTML = "Results: " + nDati;
+          s += "</table>";
+          mappaPunti();
+      document.getElementById("results").innerHTML = "Results: " + nrighe;
       document.getElementById("content").innerHTML = s;
     });
   });
 }
+
+
+
+function mappaPunti() {
+  var frame = document.getElementById('mappa');
+  frame.contentWindow.postMessage(puntitrovati, '*');
+}
+
+function createTable() {
+  $(document).ready(function () {
+    var yearMonth = document.getElementById("month").value; // yyyy-mm
+    var url =
+      "http://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=" + yearMonth + "-01&endtime=" + yearMonth + "-" + days();
+    // "http://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=2021-01-01&endtime=2021-01-02"
+    $.ajax({
+      url,
+    }).then(function (data) {
+      puntitrovati=new Array();
+      var nDati;
+      var s = "<table border= 1px>";
+      s += "<tr> <th>Date</th> <th>Place</th> <th>Magnitude</th></tr>";
+      nDati = data.features.length;
+      var nrighe = 0;
+
+      for (i = 0; i < nDati; i++) {
+        var sec = data.features[i].properties.time;
+        var dateComplete = new Date(sec);
+        var date = dateComplete.toString().slice(3, 24);
+        var mag = document.getElementById("magnitude").value;
+        var valMag = parseFloat(mag);
+        var range = document.getElementById("range").value;
+        var valRange =  parseFloat(range);
+        var stato = document.getElementById("state").value;
+       
+        
+        if (stato && !data.features[i].properties.place.toUpperCase().includes(stato.toUpperCase())) {
+          continue;
+        }
+
+        if (data.features[i].properties.mag <= (valMag + valRange) && data.features[i].properties.mag >= (valMag - valRange)) {
+          s += "<tr><td>" + 
+          date +"</td><td>" + 
+          data.features[i].properties.place + "</td><td>" +
+          data.features[i].properties.mag + "</td></tr>";
+          nrighe ++;
+          puntitrovati.push(data.features[i].geometry.coordinates[0])
+          puntitrovati.push(data.features[i].geometry.coordinates[1])
+        }  
+
+        
+      }
+      s += "</table>";
+      mappaPunti();
+      document.getElementById("results").innerHTML = "Results: " + nrighe;
+      document.getElementById("content").innerHTML = s;
+    });
+  });
+}
+
+function datestable() {
+  $(document).ready(function () {
+    
+    var date1 = document.getElementById("date1").value; // yyyy-mm-dd
+    var date2 = document.getElementById("date2").value;
+
+    var day1 = date1.slice(8, 10);
+    var day2 = date2.slice(8, 10);
+
+    var yearMonth1 = date1.slice(0, 7);
+    var yearMonth2 = date2.slice(0, 7);
+
+    console.log(yearMonth1);
+
+    var url =
+      "http://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=" + yearMonth1 + "-"+ day1 +"&endtime=" + yearMonth2 + "-" + day2;
+    // "http://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=2021-01-01&endtime=2021-01-02"
+    $.ajax({
+      url,
+    }).then(function (data) {
+      puntitrovati=new Array();
+      var nDati;
+      var s = "<table border= 1px>";
+      s += "<tr> <th>Date</th> <th>Place</th> <th>Magnitude</th></tr>";
+      nDati = data.features.length;
+      var nrighe = 0;
+
+      for (i = 0; i < nDati; i++) {
+        var sec = data.features[i].properties.time;
+        var dateComplete = new Date(sec);
+        var date = dateComplete.toString().slice(3, 24);
+        var mag = document.getElementById("magnitude").value;
+        var valMag = parseFloat(mag);
+        var range = document.getElementById("range").value;
+        var valRange =  parseFloat(range);
+       
+          s += "<tr><td>" + 
+          date +"</td><td>" + 
+          data.features[i].properties.place + "</td><td>" +
+          data.features[i].properties.mag + "</td></tr>";
+          nrighe ++;
+          puntitrovati.push(data.features[i].geometry.coordinates[0])
+          puntitrovati.push(data.features[i].geometry.coordinates[1])
+      }
+      s += "</table>";
+      mappaPunti();
+      document.getElementById("results").innerHTML = "Results: " + nrighe;
+      document.getElementById("content").innerHTML = s;
+    });
+  });
+}
+
+
 
 
